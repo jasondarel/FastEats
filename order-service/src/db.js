8c603d14134e26726c50b6a5597854d7ca9c83b7
jasondarel <jdarel21@gmail.com>
2025-02-11 17:23:06 +0700

    Nyambungin db

--- order-service/src/db.js
+++ order-service/src/db.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -7,6 +8,15 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
+});
+
+console.log("DB Config: ", {
+  //ngetes
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD ? "********" : "NOT SET",
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
