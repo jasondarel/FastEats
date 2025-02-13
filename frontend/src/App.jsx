c2d4e8321de5896c249365bce74e7bdc4e82c320
jasondarel <jdarel21@gmail.com>
2025-02-13 17:56:20 +0700

    Create ProtectedRoute.jsx
    users must login to access other pages, Changed App.jsx a bit to implement ProtectedRoute.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProfilePage from "./pages/ProfilePage";
+import ProtectedRoute from "./components/ProtectedRoute";
 import React from "react";
 
 function App() {
@@ -10,11 +11,30 @@ function App() {
     <div className="flex">
       <div className="flex-1 p-4">
         <Routes>
+          {/* Redirect "/" to "/home" */}
           <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
+
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<ProfilePage />} />
+
+          {/* Protected Routes (Require Login) */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </div>
