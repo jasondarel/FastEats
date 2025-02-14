47d43856e0da7f69892fd5ed8be5f34088d351c8
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-02-14 17:00:02 +0700

    Misahin controller sama router

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
-import axios from "axios";
 import dotenv from "dotenv";
+import {
+    getRestaurantsController,
+    loginController
+} from "./controllers/controller.js"
 
 dotenv.config();
 
@@ -20,67 +23,9 @@ console.log(
   `${process.env.SERVICE_NAME || "User/Auth Service"} running on port ${PORT}`
 );
 
-app.get("/restaurant/restaurants", async (req, res) => {
-    try {
-        const token = req.headers.authorization;
-      const response = await axios.get(
-        `${process.env.RESTAURANT_SERVICE_URL}/restaurants`,
-        {
-          headers: {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-  
-      if (!response.data) {
-        return res.status(500).json({
-          success: false,
-          message: "Invalid response from the external API",
-        });
-      }
-  
-      return res.status(200).json({
-        success: true,
-        data: response.data,
-      });
-    } catch (err) {
-      console.error("Error fetching restaurants:", err.message);
-  
-      if (err.response) {
-        return res.status(err.response.status || 500).json({
-          success: false,
-          message: err.response.data || "Error from external API",
-        });
-      }
-  
-      return res.status(500).json({
-        success: false,
-        message: "Internal Server Error",
-      });
-    }
-  });  
+app.get("/restaurant/restaurants", getRestaurantsController);  
 
-  app.post("/user/login", async (req, res) => {
-    const userReq = req.body;
-
-    if (!userReq || !userReq.email || !userReq.password) {
-        return res.status(400).json({ error: "email and password are required!" });
-    }
-
-    try {
-        const response = await axios.post(`${process.env.USER_SERVICE_URL}/login`, userReq);
-        
-        if (response.data && response.data.token) {
-            return res.status(200).json({ token: response.data.token });
-        } else {
-            return res.status(500).json({ error: "Invalid response from authentication server" });
-        }
-    } catch (err) {
-        console.error("Login error:", err.message);
-        return res.status(500).json({ error: "Login failed. Please try again later." });
-    }
-});
+  app.post("/user/login", loginController);
 
 
 app.listen(PORT, () => {
