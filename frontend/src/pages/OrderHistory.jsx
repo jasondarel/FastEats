a7a48d6ca4e6b6e0c3be3de296ffa4e9ea483868
stevan <stevan@STEVAN>
2025-02-21 17:40:39 +0700

    fixing border on mymeny detail and order history

--- frontend/src/pages/OrderHistory.jsx
+++ frontend/src/pages/OrderHistory.jsx
@@ -20,65 +20,7 @@ const OrderHistory = () => {
         {/* Scrollable Order List */}
         <div className="w-full max-w-lg h-[calc(100vh-120px)] overflow-y-auto p-3 lg:scale-125 lg:mt-10 lg:max-h-[580px] xl:mt-13 xl:scale-[1.3] xl:max-h-[570px]">
           {/* Order Item */}
-          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300">
-            <div className="flex justify-between gap-x-2 md:gap-x-6 lg:gap-x-60">
-              <div className="flex">
-                <div className="flex items-center mr-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                    />
-                  </svg>
-                </div>
-                <div>
-                  <h4 className="font-bold leading-3 text-sm">Order</h4>
-                  <p className="text-sm text-slate-700">13 Nov 2025</p>
-                </div>
-              </div>
-              <div className="flex items-center justify-center">
-                <div className="bg-green-300 px-1 py-0.5 rounded-md text-green-800 font-semibold text-sm">
-                  Completed
-                </div>
-              </div>
-            </div>
-            <hr className="my-3 border-gray-300" />
-            <div className="flex">
-              <img
-                src={image}
-                alt="product"
-                className="w-16 h-16 md:w-20 md:h-20 object-contain"
-              />
-              <div className="pt-3 pl-3 ">
-                <h2 className="font-bold truncate max-w-[200px] md:max-w-80 overflow-hidden text-ellipsis whitespace-nowrap text-base md:text-lg">
-                  Minuman Yang Sangat Mantap banget
-                </h2>
-
-                <p className="text-slate-600">1 Item</p>
-              </div>
-            </div>
-            <div className="flex justify-between mt-2 gap-x-2 md:gap-x-6 lg:gap-x-60">
-              <div className="flex flex-col">
-                <h2 className="text-sm">Total Order</h2>
-                <p className="font-bold">$ 89.99</p>
-              </div>
-              <div className="flex items-center justify-center">
-                <div className="bg-green-700 px-3 py-0.5 rounded-md text-white font-semibold text-sm">
-                  Order again
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300">
+          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300 inset-shadow-sm inset-shadow-slate-300">
             <div className="flex justify-between gap-x-2 md:gap-x-6 lg:gap-x-60">
               <div className="flex">
                 <div className="flex items-center mr-3">
@@ -136,7 +78,7 @@ const OrderHistory = () => {
             </div>
           </div>
 
-          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300">
+          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300 inset-shadow-sm inset-shadow-slate-300">
             <div className="flex justify-between gap-x-2 md:gap-x-6 lg:gap-x-60">
               <div className="flex">
                 <div className="flex items-center mr-3">
@@ -194,7 +136,7 @@ const OrderHistory = () => {
             </div>
           </div>
 
-          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300">
+          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300 inset-shadow-sm inset-shadow-slate-300">
             <div className="flex justify-between gap-x-2 md:gap-x-6 lg:gap-x-60">
               <div className="flex">
                 <div className="flex items-center mr-3">
@@ -250,9 +192,65 @@ const OrderHistory = () => {
                 </div>
               </div>
             </div>
-          </div>  
-          
-          
+          </div>
+
+          <div className="my-2 px-3 py-4 rounded-md shadow-sm shadow-slate-300 inset-shadow-sm inset-shadow-slate-300">
+            <div className="flex justify-between gap-x-2 md:gap-x-6 lg:gap-x-60">
+              <div className="flex">
+                <div className="flex items-center mr-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h4 className="font-bold leading-3 text-sm">Order</h4>
+                  <p className="text-sm text-slate-700">13 Nov 2025</p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="bg-green-300 px-1 py-0.5 rounded-md text-green-800 font-semibold text-sm">
+                  Completed
+                </div>
+              </div>
+            </div>
+            <hr className="my-3 border-gray-300" />
+            <div className="flex">
+              <img
+                src={image}
+                alt="product"
+                className="w-16 h-16 md:w-20 md:h-20 object-contain"
+              />
+              <div className="pt-3 pl-3 ">
+                <h2 className="font-bold truncate max-w-[200px] md:max-w-80 overflow-hidden text-ellipsis whitespace-nowrap text-base md:text-lg">
+                  Minuman Yang Sangat Mantap banget
+                </h2>
+
+                <p className="text-slate-600">1 Item</p>
+              </div>
+            </div>
+            <div className="flex justify-between mt-2 gap-x-2 md:gap-x-6 lg:gap-x-60">
+              <div className="flex flex-col">
+                <h2 className="text-sm">Total Order</h2>
+                <p className="font-bold">$ 89.99</p>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="bg-green-700 px-3 py-0.5 rounded-md text-white font-semibold text-sm">
+                  Order again
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
