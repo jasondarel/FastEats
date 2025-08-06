eb75772f951864014b1ed5dc6aaa10eb66d35881
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-06 11:40:08 +0700

    Update: add addsOnDataField payload

--- frontend/src/service/restaurantService/menuDetailsService.js
+++ frontend/src/service/restaurantService/menuDetailsService.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { API_URL } from "../../config/api";
 
-const insertOrderService = async (menuId, quantity, token) => {
+const insertOrderService = async (menuId, quantity, token, addsOnData) => {
   const response = await axios.post(
     `${API_URL}/order/order`,
     {
       menuId: menuId,
       quantity: quantity,
+      addsOnData: addsOnData,
     },
     {
       headers: {
