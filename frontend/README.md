46eb1a73a5c366d5834c9f9b31d8f1745ceda800
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-16 19:53:04 +0700

    Remove: remove frontend

--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-npm install axios react-router-dom
-(https://tailwindcss.com/docs/installation/using-vite) //ikutin buat download tailwind
-npm install react-icons
--- frontend/src/store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './app/auth/authSlice';
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    }
-})
