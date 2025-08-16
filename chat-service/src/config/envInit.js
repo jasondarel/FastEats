301f9276838432039faf64de109b6962c6f72311
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-16 19:22:30 +0700

    Delete: delete chat service

--- chat-service/src/config/envInit.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-const envInit = () => {
-    const envFile = process.env.NODE_ENV === "production" ? ".env.prod" : ".env.dev";
-    dotenv.config({ path: envFile });
-}
-
-export default envInit;
