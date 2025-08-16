965b62a1f765933b6cb375b213c4234c381a31c0
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-16 19:30:44 +0700

    Remove: remove order service

--- order-service/src/config/envInit.js
+++
@@ -1,9 +0,0 @@
-import logger from "./loggerInit.js";
-import dotenv from "dotenv";
-
-const envInit = () => {
-    const envFile = process.env.NODE_ENV === "production" ? ".env.prod" : ".env.dev";
-    dotenv.config({ path: envFile });
-}
-
-export default envInit;
